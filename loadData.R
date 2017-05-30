54a5bf9c8ae77b30267342c8d85a6c8ded4e5ebe
kam109 <kam109@duke.edu>
2017-05-30 00:38:34 -0400

    added a bit of quality control

--- loadData.R
+++ loadData.R
@@ -2,27 +2,51 @@ library(minfi)
 library(minfiData)
 library(IlluminaHumanMethylationEPICmanifest)
 library(IlluminaHumanMethylationEPICanno.ilm10b2.hg19)
+library(gdata)
+
+
+## Read in data
 
 # here set a working directory in the folder with EPIC data
 # specifically the directory containing folders for 1337, 1345, etc
 setwd("/Users/kevinmurgas/Documents/Data+ project/EPIC data")
 
-# start by choosing 1337
+# start by choosing 1337 as base directory
 baseDir <- "1337_Shibata EPIC DNA methylation data package/IDAT FILES"
 
+# read in BaseNames from the .csv file
 targets <- read.csv("1337_Shibata EPIC DNA methylation data package/SAMPLE-ARRAY MAPPING/1337_Shibata-targets.csv", as.is = TRUE)
 
-sub(baseDir, "", targets$Basename)
-
-targets$Basename <- file.path(path, targets$Basename)
+# converts target files to RGset
+targets$Basename <- file.path(baseDir, targets$Basename)
 RGset <- read.metharray(targets$Basename, verbose = TRUE)
+# annotation(RGsetEx) # check annotation packages
 
-# insert quality control
+# read in patient data from the .xls file
+pd <- read.xls("1337_Shibata EPIC DNA methylation data package/SAMPLE-ARRAY MAPPING/1337 (Shibata-8).xls", sheet = 1, header = TRUE)
+
+
+## Quality Control
+
+# vvv this line does not work ¯\_(ツ)_/¯
+# qcReport(RGset, sampNames = pd$Sample_No, sampGroups = pd$Plate, pdf = "qcReport.pdf")
+
+# this line works to show density curves :)
+densityPlot(RGset, sampGroups = pd$Sample_No, main = "Beta", xlab = "Beta")
+
+# insert more QC
+
+
+## Pre-processing
 
 # built-in preprocessing, we should do this ourselves
 mset <- preprocessIllumina(RGset)
 mset <- mapToGenome(mset)
 
 dim(getBeta(mset, type = "Illumina"))  ##the argument type='Illumina' gives us default procedure
+head(granges(mset))
 
+sex <- getSex(mset)
+plotSex(sex)
 
+plot(as.matrix(getQC(mset)))

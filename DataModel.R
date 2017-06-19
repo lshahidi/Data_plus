cbdd2c38c2f90db5a3aed300cbe518d89f2179d5
Yanlin <yanlin.ma@duke.edu>
2017-06-19 11:18:38 -0400

    Function used for reading in different sites created

--- DataModel.R
+++ DataModel.R
@@ -5,11 +5,13 @@
 ### INITIALIZE
 
 # install.packages("coda")
+# install.packages("gtools")
 
 library(lme4)
 library(arm)
 library(rstan)
 library(coda)
+library(gtools)
 
 # here set the working directory that points to the data folder
 # e.g. the folder with annotated data saved as "myFA.Rdata"
@@ -28,53 +30,44 @@ setwd("D:/DataPlus2017/Data")
 # load fully annotated data (saved from LoadDataAndQC.R)
 load("myFA.Rdata")
 
-# extract CpG site 1 to start
-firstSite <- head(FullAnnotation, 1)
 
-# convert first site to table, using only H E K* W C J (tumor+normal)
-# 18 rows, with columns for patient, tumor/normal
-# index in order A,B,N
-indices <- c(11, 12, 13, 17, 18, 16, 20, 21, 19, 25, 26, 24, 33, 34, 32, 
-             39, 31, 15)
-patientLabel <- c(rep("H", 3),rep("E", 3),rep("K*", 3),rep("W", 3),rep("C", 3),
-                  rep("J", 3))
-tissueLabel <- rep(c("T", "T", "N"), 6)
-sideLabel <- rep(c("A", "B", ""), 6)
+# Function used to read in data from each site
 
-firstData <- data.frame(beta=logit(t(firstSite[indices])), patient = patientLabel, tissue=tissueLabel, side=sideLabel)
+site <- function (site_no) {
+  # extract CpG site xx to start
+  temp <- FullAnnotation[site_no,]
+  
+   # here we use all patient samples, excluding glands
+  indices <- c(9:13, 15:39,46,47,57,58,72:75)
+  patientLabel <- substr(colnames(temp[indices]),1,1)
+  patientLabel[10:12] <- "K*"
+  sideLabel <- substr(colnames(temp[indices]),2,2)
+  tissueLabel <- sideLabel
+  tissueLabel[tissueLabel %in% c("A","B")] <- "T"   #replace A and B with T
+  tumorIndicator <- 1*(tissueLabel=="T")
+  
+  work <- data.frame(beta=logit(t(temp[indices])), patient = patientLabel, tissue=tissueLabel, side=sideLabel, tInd=tumorIndicator)
+  
+  return(work)
+}
 
-
-# here we use all patient samples, excluding glands
-indices <- c(9:13, 15:39,46,47,57,58,72:75)
-patientLabel <- substr(colnames(firstSite[indices]),1,1)
-patientLabel[10:12] <- "K*"
-sideLabel <- substr(colnames(firstSite[indices]),2,2)
-tissueLabel <- sideLabel
-tissueLabel[tissueLabel %in% c("A","B")] <- "T"   #replace A and B with T
-tumorIndicator <- 1*(tissueLabel=="T")
-
-firstData <- data.frame(beta=logit(t(firstSite[indices])), patient = patientLabel, tissue=tissueLabel, side=sideLabel, tInd=tumorIndicator)
-
-
-# here we use only patients with glands
-indices <- c(9:13,32:34,57,58,60:71,76:79)
-patientLabel <- substr(colnames(firstSite[indices]),1,1)
-sideLabel <- substr(colnames(firstSite[indices]),2,2)
-tissueLabel <- sideLabel
-tissueLabel[tissueLabel %in% c("A","B")] <- "T"   #replace A and B with T
-tumorIndicator <- 1*(tissueLabel=="T")
-glandIndicator <- 1*(indices %in% c(60:71,76:79))
-
-firstData <- data.frame(beta = logit(t(firstSite[indices])), patient = patientLabel, tissue=tissueLabel, side=sideLabel, tInd=tumorIndicator, gInd=glandIndicator)
+Data1 <- site(1)
+Data2 <- site(2)
+Data3 <- site(3)
+Data4 <- site(4)
+Data5 <- site(5)
+Data6 <- site(6)
+Data7 <- site(7)
+Data8 <- site(8)
 
 
 
 
 ### FIT WITH LMER
 
-fit1 <- lmer(X1 ~ tInd + (tInd|patient), firstData)
+fit1 <- lmer(X1 ~ tInd + (tInd|patient), Data1)
 
-fitGland <- lmer(X1 ~ tInd + (tInd|patient) + (0+gInd|patient:side), firstData)
+fitGland <- lmer(X1 ~ tInd + (tInd|patient) + (0+gInd|patient:side), Data1)
 
 patLabs <- c("C", "D","E","F","H","J","K","K*","M","O","P","S","T","U","W","X")
 # barplot(fixef(fit1), main="Fixed Effects", xlab="Effect", ylab="Intercept Estimate", names.arg=c("mu","betaT"))
@@ -102,16 +95,16 @@ barplot(ranef(fit2)$patient$'(Intercept)'+ranef(fit2)$patient$tInd, main="Both R
 
 # Logit transform on beta values
 
-#firstData$y <- log(firstData$X1/(1-firstData$X1))
-firstData$y <- firstData$X1
+#Data1$y <- log(Data1$X1/(1-Data1$X1))
+Data1$y <- Data1$X1
 
 # Stan data
 
-stanDat <- list(pID = as.integer(factor(firstData$patient)),
-                tInd = firstData$tInd,
-                N = nrow(firstData),
-                P = nlevels(firstData$patient),
-                y = firstData$y)
+stanDat <- list(pID = as.integer(factor(Data1$patient)),
+                tInd = Data1$tInd,
+                N = nrow(Data1),
+                P = nlevels(Data1$patient),
+                y = Data1$y)
 
 
 stanFit <- stan(file="model.stan",data=stanDat)

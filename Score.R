a9026fcc813940b26e9b94b75bf4a3f38395aa82
Yanlin <yanlin.ma@duke.edu>
2017-07-24 10:23:53 -0400

    Score sites using posterior log ratio

---
+++ Score.R
@@ -0,0 +1,81 @@
+########################## Scoring ################################
+
+
+library(rstan)
+library(gtools)
+library(bayesplot)
+
+
+# here set the working directory that points to the data folder
+# e.g. the folder with annotated data saved as "myFA.Rdata"
+# please just comment out the directories already here, so each
+# user can uncomment the setwd for them as we move code around
+
+# Kevin's working directory
+setwd("/Users/kevinmurgas/Documents/Data+ project/EPIC data")
+
+# Yanlin's working directory
+setwd("D:/DataPlus2017/Data")
+
+
+### LOAD DATA
+
+# load fully annotated data (saved from LoadDataAndQC.R)
+load("myFA.Rdata")
+
+
+
+# Function used to read in data from each site
+
+site <- function (site_no) {
+  # extract CpG site xx to start
+  temp <- FullAnnotation[site_no,]
+  
+  # here we use all patient samples, excluding glands
+  indices <- c(9:13, 15:39,46,47,57,58,72:75)
+  patientLabel <- substr(colnames(temp[indices]),1,1)
+  patientLabel[10:12] <- "K*"
+  sideLabel <- substr(colnames(temp[indices]),2,2)
+  tissueLabel <- sideLabel
+  tissueLabel[tissueLabel %in% c("A","B")] <- "T"   #replace A and B with T
+  tumorIndicator <- 1*(tissueLabel=="T")
+  
+  work <- data.frame(beta=logit(t(temp[indices])), patient = patientLabel, tissue=tissueLabel, side=sideLabel, tInd=tumorIndicator)
+  
+  return(work)
+}
+
+
+# Using Model 3: add intra-tumoral variance
+
+stanfit3 <- function (dataset) {
+  
+  stanDat <- list(pID = as.integer(factor(dataset$patient)),
+                  tInd = dataset$tInd,
+                  N = nrow(dataset),
+                  P = nlevels(dataset$patient),
+                  y = dataset[,1])
+  
+  
+  
+  stanFit3 <- stan(file="model3.stan", data=stanDat, control = list(adapt_delta = 0.999))
+  
+  return(stanFit3=stanFit3)
+}
+
+
+
+prob <- rep(NA,dim(FullAnnotation)[1])
+
+for (i in dim(FullAnnotation)[1]) {
+  
+  data <- site(i)
+  stan <- stanfit3(data)
+  
+  posterior <- as.matrix(stan,pars=c("sigma_p","sigma_t"))
+  
+  PTratio <- log(sigma[,1]/sigma[,2])
+  
+  prob[i] <- sum(PTratio1 > 0)/length(PTratio)
+  
+}
